42970cf8fd331815a7cb0d2640a1e3f91ccbe0bc
ace1110 <liaojiayouy@gmail.com>
2026-08-28 11:35:27 +0800

    rename binary

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ chcp 65001 >nul
 
 pyinstaller --clean --onedir --noconfirm --exclude-module PyQt5 --exclude-module PyQt6 --windowed --icon=main_icon.ico --name=main_x64 --distpath=./ main.py
 
+ren main_x64\main_x64.exe yoCryptEditor.exe
 copy password.txt main_x64\
 copy Welcome.txt main_x64\
 
