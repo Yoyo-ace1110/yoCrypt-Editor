d9661b5b009bc0dfe6dcf87405cc379cf4e13365
ace1110 <liaojiayouy@gmail.com>
2026-08-28 11:32:19 +0800

    some bugs fixed in build scripts and update binarys for windows

--- build.bat
+++ build.bat
@@ -1,11 +1,7 @@
 @echo off
 chcp 65001 >nul
 
-pyinstaller --clean --onedir --exclude-module PyQt5 --exclude-module PyQt6 --windowed --icon=main_icon.ico --name=yoCryptEditor --distpath=./ main.py
-
-if exist yoCryptEditor (
-    ren yoCryptEditor main_x64
-)
+pyinstaller --clean --onedir --noconfirm --exclude-module PyQt5 --exclude-module PyQt6 --windowed --icon=main_icon.ico --name=main_x64 --distpath=./ main.py
 
 copy password.txt main_x64\
 copy Welcome.txt main_x64\
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pyinstaller --clean --onedir --exclude-module PyQt5 --exclude-module PyQt6 --windowed --icon=main_icon.ico --name=yoCryptEditor --distpath=./ main.py
+pyinstaller --clean --onedir --noconfirm --exclude-module PyQt5 --exclude-module PyQt6 --windowed --icon=main_icon.ico --name=main_x64 --distpath=./ main.py
 
 if [ -d "yoCryptEditor" ]; then
     mv yoCryptEditor main_linux
